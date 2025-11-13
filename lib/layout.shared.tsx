1aca9deb2547cf2e3355e2753efe8074aeeced9d
SeaotterMS <peter910820@gmail.com>
2025-11-13 22:36:47 +0800

    feat: update docs to v1.9.0

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -3,7 +3,7 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "KuroHelper-v1.8.X",
+      title: "KuroHelper-v1.9.X",
       transparentMode: "top",
     },
   };
