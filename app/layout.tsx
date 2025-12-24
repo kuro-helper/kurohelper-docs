cb12c2a4072ee58fe023b07a586586270ad5f300
SeaotterMS <peter910820@gmail.com>
2025-12-24 21:08:31 +0800

    feat: update docs to v1.11.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
+import { Banner } from 'fumadocs-ui/components/banner';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -19,6 +20,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="zh-Hant" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Banner>更新文檔也需要時間，所以建議還是加入官方討論群才能得到第一手消息</Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
